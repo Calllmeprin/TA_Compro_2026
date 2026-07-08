e4781ad2ac945135a03b6575143ea918afb007a4
Preeyapat Pongthawepath <princepongthaveepath@gmail.com>
2026-07-08 15:07:46 +0000

    Edit Challenge

--- lab1/cha.c
+++ lab1/cha.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int main () {
-    printf ("----------------+--------+---------+\n");
-    printf ("| Name           | Score1 | Score2 |\n");
-    printf ("+----------------+--------+--------+\n");
-    printf ("| Alice          |     85 |     90 |\n");
-    printf ("| Bob            |     78 |     82 |\n");
-    printf ("| Charlie        |     92 |     88 |\n");
-    printf ("+----------------+--------+--------+\n");
+int main()
+{
+    printf("----------------+--------+--------+\n");
+    printf("%-16s| %6s | %6s |\n", "Name", "Score1", "Score2");
+    printf("----------------+--------+--------+\n");
+    printf("%-16s| %6d | %6d |\n", "Alice", 85, 90);
+    printf("%-16s| %6d | %6d |\n", "Bob", 78, 82);
+    printf("%-16s| %6d | %6d |\n", "Charlie", 92, 88);
+    printf("----------------+--------+--------+\n");
 
     return 0;
-
 }
---
+++ lab1/scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    char name [40];
+    float gpa;
+    printf ("Enter Your Name: ");
+    scanf ("%s", name);
+    printf ("Enter Your GPA: ");
+    scanf ("%f", &gpa);
+    return (0);
+}
